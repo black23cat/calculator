a5118d9d51f29702000f4a7c77a7ff381c5c89ca
Prayogi Pangestu <fmother731@gmail.com>
2024-09-03 10:05:27 +0700

    Add operate function

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const btmScreen = document.querySelector('.btmScreen');
 const operandBtn = document.querySelectorAll('.operand');
 const operatorBtn = document.querySelectorAll('.operator');
 const assignBtn = document.querySelector('.asign');
+
+function operate(number1, number2, operator){
+    let a = number1;
+    let b = number2;
+    num2 = '';
+    switch(operator){
+      case '+':
+        console.log(num1)
+        return a + b;
+      case '-':
+        console.log(num1)
+        return a - b;
+      case '*':
+        console.log(num1)
+        return a * b;
+      case '/':
+        console.log(num1)
+        return a / b;
+    }
+  }
