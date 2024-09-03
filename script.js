710df4d6b89ed2fef2a937103326e722331762a6
Prayogi Pangestu <fmother731@gmail.com>
2024-09-03 10:02:15 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const btmScreen = document.querySelector('.btmScreen');
+const operandBtn = document.querySelectorAll('.operand');
+const operatorBtn = document.querySelectorAll('.operator');
+const assignBtn = document.querySelector('.asign');
