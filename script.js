c746e3eb62cec718ca25e9d1bd1c8f4d5ed20a70
Prayogi Pangestu <fmother731@gmail.com>
2024-09-05 11:27:48 +0700

    Fix screen when user press operate button before entering number Prevent calculator from calculating number divise by 0 Fix calculator screen overflow

--- script.js
+++ script.js
@@ -9,61 +9,69 @@ const assignBtn = document.querySelector('.asign');
 const topScreen = document.querySelector('.topScreen');
 const comma = document.querySelector('.comma');
 const allClearBtn = document.querySelector('.clear');
-btmScreen.textContent = '0';
+defaultScreen();
 
 function operate(number1, number2, operator){
-    let a = parseFloat(number1);
-    let b = parseFloat(number2);
-    num2 = '';
-		btmScreen.textContent = `${num2}`
+    let result;
     switch(operator){
       case '+':
-        return a + b;
+        result = parseFloat(number1) + parseFloat(number2);
+        break;
       case '-':
-        return a - b;
+        result = number1 - number2;
+        break;
       case '*':
-        return a * b;
+        result = number1 * number2;
+        break;
       case '/':
-        return a / b;
+        result = number1 / number2;
+        break;
     }
-		console.log(num1);
+    return (result %1 !== 0)? `${result.toFixed(1)}`
+          :(String(result).length > 11)? `${result.toExponential(6)}`
+          :`${result}`;
+
   }
 
 operandBtn.forEach(button =>{
   button.addEventListener('click', ()=>{
-		if(num2.length < 9){
-			num2 += button.value;
-			btmScreen.textContent = `${num2}`;
+		if(num2.length <= 10){
+      if(button.value == '0' && num2.length == 0){
+        defaultScreen()
+      }else{
+        num2 += button.value;
+        btmScreen.textContent = `${num2}`;
+      }
 		}
 	});
 });
 
 operatorBtn.forEach(e => {
   e.addEventListener('click', () =>{
-		if(num1 == '' && num2 == ''){
-			btmScreen.textContent = '????????';
+		if(num1.length == 0 && num2.length == 0){
+			btmScreen.textContent = '??????????';
 		}else if(num1 == ''){
       num1 = num2;
 			num2 = ''
       operator = e.value;
+      defaultScreen()
     }else if(num2 == ''){
       operator = e.value;
     }else{
-    num1 = `${operate(num1, num2, operator)}`;  
-		operator = e.value;  
-    console.log(num1);
+      num1 = operate(num1, num2, operator);  
+      operator = e.value; 
+      num2 = '';
+      defaultScreen()
     }
-		btmScreen.textContent = '0';
 		topScreen.textContent = `${num1} ${operator}`;
   });
 });
 
 assignBtn.addEventListener('click', ()=>{
-	if(operator == '/' && num2 < '1' && num2 < '-1'){
-		btmScreen.textContent = 'LoL XD XD';
-		topScreen.textContent = '';
+	if(operator == '/' && num2 < '1' && num2 > '-1'){
 		clearCalculator();
-	}else if(num1.length > 0 && num2.length > 0){
+    btmScreen.textContent = 'LoL XD XD';
+  }else if(num1.length > 0 && num2.length > 0){
 		let result = operate(num1, num2, operator);
 		clearCalculator();
 		btmScreen.textContent = `${result}`;
@@ -71,7 +79,6 @@ assignBtn.addEventListener('click', ()=>{
 });
 
 comma.addEventListener('click', ()=>{
-	console.log(comma.value)
 	if(num2.length == 0 && !(num2.includes('.'))){
 		num2 += `0${comma.value}`;
 		btmScreen.textContent = `${num2}`
@@ -90,5 +97,9 @@ function clearCalculator(){
 	num2 = '';
 	operator = '';
 	topScreen.textContent = '';
-	btmScreen.textContent = '0';
+  defaultScreen();
+}
+
+function defaultScreen(){
+  btmScreen.textContent = '0';
 }
